2d46548a8e4a1781c07a6b555382db8ebe04ef14
Brianna <brianna.karpowicz@gmail.com>
2018-10-21 22:07:13 -0400

    loops through tasks, checks that patient and tasks have been inputted

--- gui.py
+++ gui.py
@@ -1,10 +1,18 @@
 import tkinter as tk
+import time
+from glob import glob
+import datetime
 
 global task_selections
 task_selections = {}
 
 global current_patient
 current_patient = None
+
+global data_dir 
+data_dir= 'Patient_Metrics'
+
+## TODO: os.mkdir(path)
 
 def openpose_capture(): 
     # From Python
@@ -26,6 +34,59 @@ def openpose_capture():
         import openpose as op
     except:
         raise Exception('Error: OpenPose library could not be found.')
+
+    #set up filesystem 
+    #if patient is still None, then raise a warning window 
+    session = datetime.datetime.now().strftime("%Y-%m-%d-%H%M")
+
+    if len(task_selections) <= 0: 
+        win = tk.Toplevel() 
+        win.geometry('400x100')
+        win.wm_title("No Task Selection")
+
+        l = tk.Label(win, text = "Please select at least one task.")
+        l.grid(row = 0, column = 0)
+        b = tk.Button(win, text = 'OK', 
+                command = win.destroy)
+        b.grid(row = 1, column = 0)
+
+        win.wait_window(window = win)
+        return
+    else: 
+        new_dict = {}
+        for t in task_selections.keys(): 
+            val = task_selections[t]
+            t = t.lower().replace(' ', '')
+            new_dict[t] = val
+        globals().update(task_selections = new_dict)
+        # print(task_selections)
+
+    # return 
+
+    if current_patient == None: 
+        win = tk.Toplevel()
+        win.geometry('400x100')
+        win.wm_title("No Patient Selected")
+
+        l = tk.Label(win, text = "Please enter a new patient or select an existing patient!")
+        l.grid(row = 0, column = 0)
+        b = tk.Button(win, text = "OK",
+                command = win.destroy)
+        b.grid(row = 1, column = 0)
+
+        win.wait_window(window = win)
+        return
+    else: 
+        #if patient is selected, then check to see if there is already a directory for them 
+        #if not, then make one 
+        patient_dirs = glob(data_dir + "/*/")
+        print(patient_dirs)
+        if data_dir + '/' + current_patient + '/' not in patient_dirs: 
+            os.makedirs(data_dir + '/' + current_patient)
+        savepath = data_dir + '/' + current_patient + '/' + session
+        os.makedirs(savepath)
+
+    #within the directory, make one for the new date/time for session 
 
     params = dict()
     params["logging_level"] = 3
@@ -53,49 +114,59 @@ def openpose_capture():
     w = cap.get(cv2.CAP_PROP_FRAME_WIDTH);
     h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT); 
 
-    # Define the codec and create VideoWriter object to save the video
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    video_writer_original = cv2.VideoWriter('raw_output.avi', fourcc, 20.0, (int(w), int(h)))
-    video_writer_openpose = cv2.VideoWriter('openpose_output.avi', fourcc, 20.0, (int(w), int(h)))
+    for task in task_selections.keys(): 
+        for i in range(0, int(task_selections[task])): 
+            #loops through, save videos
+            # pass 
 
-    frame_store = []
-    point_store = []
+            # Define the codec and create VideoWriter object to save the video
+            fourcc = cv2.VideoWriter_fourcc(*'XVID')
+            video_writer_original = cv2.VideoWriter(savepath + '/raw_output_' + task + '_' + str(i) + '.avi', fourcc, 20.0, (int(w), int(h)))
 
-    recording = True
-    while(recording):
-        # Capture frame-by-frame    
-        ret, frame = cap.read()
-        # Display the frame
-        cv2.imshow('frame',frame)
-        frame_store.append(frame)
-        video_writer_original.write(frame)
-        k = cv2.waitKey(1)
-        if k is 32: #press space to quit 
-            break
+            frame_store = []
+            point_store = []
+
+            recording = True
+            while(recording):
+                # Capture frame-by-frame    
+                ret, frame = cap.read()
+                # Display the frame
+                cv2.imshow('frame',frame)
+                frame_store.append(frame)
+                video_writer_original.write(frame)
+                k = cv2.waitKey(1)
+                if k is 32: #press space to quit 
+                    break
 
     # When everything done, release the capture
     cap.release()
     video_writer_original.release()
     cv2.destroyAllWindows()
 
-    counter = 0;
-    for f in frame_store: 
-        counter += 1
-        progress = float(counter)/len(frame_store)
-        if np.floor(progress % 10) == 0:
-            print(str(progress * 100) + '% Complete' )
-        # Our operations on the frame come here
-        # This is the slow part
-        keypoints, output_frame = openpose.forward(f, True)
-        point_store.append(keypoints)
-        video_writer_openpose.write(output_frame)
+    videos = glob(savepath + '/*.avi')
 
-    video_writer_openpose.release()
+    for v in videos: 
+        filename = v.split('.')
+        video_writer_openpose = cv2.VideoWriter(savepath + '/' + filename[0] + 'openpose' + '.avi', fourcc, 20.0, (int(w), int(h)))
 
-    # Save points 
-    with open('openpose_points.csv', 'wb') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',')
-        writer.writerow(point_store)
+        counter = 0;
+        for f in frame_store: 
+            counter += 1
+            progress = float(counter)/len(frame_store)
+            if np.floor(progress % 10) == 0:
+                print(str(progress * 100) + '% Complete' )
+            # Our operations on the frame come here
+            # This is the slow part
+            keypoints, output_frame = openpose.forward(f, True)
+            point_store.append(keypoints)
+            video_writer_openpose.write(output_frame)
+
+        video_writer_openpose.release()
+
+        # Save points 
+        with open(savepath + filename[0] + '.csv', 'wb') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow(point_store)
 
 def save_selections(window, checkvars, checkboxes, inputs): 
     for i in range(0, len(checkboxes)):  
@@ -147,15 +218,18 @@ def select_tasks():
     finish_button.grid(row = 10, column = 2)
 
 def select_old_patient(window, menu): 
-    current_patient = menu.get()
+    set_current_patient(menu.get())
     window.destroy()
     # print(current_patient)
 
 def oldpatient(): 
     oldpatients = []
-    f = open('patients.txt', 'r')
+    f = open(data_dir + '/patients.txt', 'r')
     for patient in f.readlines():
-        oldpatients.append(patient)
+        patient = patient.strip('\n')
+        if patient != '': 
+            oldpatients.append(patient)
+    # print(oldpatients)
     f.close()
     
     win = tk.Toplevel()
@@ -182,9 +256,10 @@ def oldpatient():
     b2.grid(row = 1, column = 0)
 
 def submit_new_patient(window, e): 
-    current_patient = e.get()
-    f = open('patients.txt', 'w') 
-    f.write(current_patient)
+    set_current_patient(e.get())
+    f = open(data_dir + '/patients.txt', 'a') 
+    # print(current_patient)
+    f.write(current_patient + '\n')
     f.close()
     window.destroy()
 
@@ -210,6 +285,11 @@ def newpatient():
                 fg = 'red',
                 command = win.destroy)
     b2.grid(row = 2, column = 0)
+
+def set_current_patient(patientname): 
+    patientname = patientname.lower().replace(' ', '')
+    # print(patientname)
+    globals().update(current_patient = patientname)
 
 root = tk.Tk()
 root.geometry('500x500') 
@@ -241,8 +321,8 @@ quit_button.grid(row = 4, column = 1, sticky = tk.NSEW)
 start_button = tk.Button(frame,
                    text="START", 
                    fg = 'blue',
-                   command = quit)
-                   #command=openpose_capture)
+                   # command = quit)
+                   command=openpose_capture)
 start_button.grid(row = 4, column = 2, sticky = tk.NSEW)
 
 root.grid_rowconfigure(0, weight=1)
