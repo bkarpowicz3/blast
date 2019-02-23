bfe3738bb862ad564560cbc18b4a48604017424f
alfalopez <47309718+alfalopez@users.noreply.github.com>
2019-02-23 02:04:49 -0500

    Quaternion to Euler-Arduino

---
+++ Hardware/quat2eul.ino
@@ -0,0 +1,31 @@
+/*Attempt to convert quaternions to euler angles
+ * https://www.tutorialspoint.com/cprogramming/c_return_arrays_from_function.htm
+ */
+#include <math.h>
+#include <Arduino.h>
+#include <stdio.h>
+
+float *eulerangles;
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+   eulerangles = quat2eul(0.6259, 0.7761, 0.07608, 0); 
+}
+
+float * quat2eul(float q0, float q1, float q2, float q3) {
+
+  float roll = degrees(atan2((2*(q0*q1+q2*q3)),(1-2*(square(q1)+square(q2))))); //arctan2 = arc tangent of y/x
+  float pitch = degrees(asin(2*(q0*q2-q3*q1)));
+  float yaw = degrees(atan2((2*(q0*q3+q1*q2)),(1-2*(square(q2)+square(q3)))));
+  float euler[3] = {roll, pitch, yaw};
+  
+  for (int i =0; i<3;++i ){
+   Serial.print(euler[i]);
+   Serial.print('\t');
+  }
+  Serial.println();
+  return euler;
+}
