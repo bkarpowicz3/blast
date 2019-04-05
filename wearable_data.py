66a22f640ef4c1913994ce394c03b0a089045550
Brianna <brianna.karpowicz@gmail.com>
2019-04-05 14:17:32 -0400

    prints to new window, closes on space bar, saves

---
+++ wearable_data.py
@@ -0,0 +1,34 @@
+import serial
+import cv2
+import numpy as np
+import re
+
+data = serial.Serial('COM3', 115200)
+
+store = []
+
+while True:
+	d = data.readline()
+	video = np.empty((480,640,4), np.uint8)
+	values = re.split(r'\t+', d)
+	values[3] = values[3].rstrip(' \t\n\r')
+
+	cv2.putText(video, values[0], (40, 50), cv2.FONT_HERSHEY_SIMPLEX, float(1), (255,255,255), 2)
+	cv2.putText(video, values[1], (40,100), cv2.FONT_HERSHEY_SIMPLEX, float(1), (255,255,255), 2)
+	cv2.putText(video, values[2], (40,150), cv2.FONT_HERSHEY_SIMPLEX, float(1), (255,255,255), 2)
+	cv2.putText(video, values[3], (40,200), cv2.FONT_HERSHEY_SIMPLEX, float(1), (255,255,255), 2)
+
+	nums = []
+	for v in values: 
+		pieces = v.split()
+		nums.append(float(pieces[2]))
+	store.append(nums)
+
+	cv2.imshow('IMU Data Stream', video)
+
+	key = cv2.waitKey(32)
+	if key == 32: 
+		break
+
+a = np.asarray(store)
+np.savetxt("imu.csv", a, delimiter=",")
