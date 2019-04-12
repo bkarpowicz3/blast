81813d0b815f7a57ab534b94e2a6a5176a259135
Brianna <brianna.karpowicz@gmail.com>
2019-04-12 13:56:30 -0400

    adjusted for hardware code changes

--- wearable_data.py
+++ wearable_data.py
@@ -5,26 +5,30 @@ import re
 
 data = serial.Serial('COM3', 115200)
 
+
 store = []
 
 while True:
 	d = data.readline()
-	video = np.empty((480,640,4), np.uint8)
-	values = re.split(r'\t+', d)
-	values[3] = values[3].rstrip(' \t\n\r')
+	# print d
+	if 'qx' in d:
+		video = np.empty((480,640,4), np.uint8)
+		values = re.split(r'\t+', d)
+		# print(values)
+		values[3] = values[3].rstrip(' \t\n\r')
 
-	cv2.putText(video, values[0], (40, 50), cv2.FONT_HERSHEY_SIMPLEX, float(1), (255,255,255), 2)
-	cv2.putText(video, values[1], (40,100), cv2.FONT_HERSHEY_SIMPLEX, float(1), (255,255,255), 2)
-	cv2.putText(video, values[2], (40,150), cv2.FONT_HERSHEY_SIMPLEX, float(1), (255,255,255), 2)
-	cv2.putText(video, values[3], (40,200), cv2.FONT_HERSHEY_SIMPLEX, float(1), (255,255,255), 2)
+		cv2.putText(video, values[0], (40, 50), cv2.FONT_HERSHEY_SIMPLEX, float(1), (255,255,255), 2)
+		cv2.putText(video, values[1], (40,100), cv2.FONT_HERSHEY_SIMPLEX, float(1), (255,255,255), 2)
+		cv2.putText(video, values[2], (40,150), cv2.FONT_HERSHEY_SIMPLEX, float(1), (255,255,255), 2)
+		cv2.putText(video, values[3], (40,200), cv2.FONT_HERSHEY_SIMPLEX, float(1), (255,255,255), 2)
 
-	nums = []
-	for v in values: 
-		pieces = v.split()
-		nums.append(float(pieces[2]))
-	store.append(nums)
+		nums = []
+		for v in values: 
+			pieces = v.split()
+			nums.append(float(pieces[2]))
+		store.append(nums)
 
-	cv2.imshow('IMU Data Stream', video)
+		cv2.imshow('IMU Data Stream', video)
 
 	key = cv2.waitKey(32)
 	if key == 32: 
